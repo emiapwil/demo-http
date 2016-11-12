9df00ee5e498765f1650c7c5d4613b785a707f64
Emiapwil (Gao Kai) <emiapwil@gmail.com>
2016-11-12 17:54:55 -0600

    Update the setup.sh
    Signed-off-by: Emiapwil (Gao Kai) <emiapwil@gmail.com>

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 
-apt-get -y install git apt-utils gcc wget
+apt-get -y install git apt-utils gcc wget unzip tar realpath
 apt-get -y install curl libcurl4-openssl-dev
 
 apt-get -y install python3 python3-pip
@@ -32,6 +32,27 @@ apt-get -y install bro
 git clone https://github.com/emiapwil/demo-http tutorial-sc16
 make -C tutorial-sc16
 
-export PATH=$PATH:$WORKDIR/tutorial-sc16/
+export PATH="$PATH:$WORKDIR/tutorial-sc16/"
+
+# Install bro script
+
+git clone https://github.com/charlierproctor/bro_http
+
+export PATH="$PATH:$WORKDIR/bro_http/"
+
+# Install the controller
+apt-get -y install software-properties-common debconf-utils
+
+add-apt-repository ppa:webupd8team/java -y
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
+
+apt-get update
+apt-get -y install oracle-java8-installer
+
+git clone https://github.com/emiapwil/demo-bootstrap
+
+export PATH="$PATH:$WORKDIR/demo-bootstrap"
+
+setup-controller.sh
 
 # Install the mininet script
