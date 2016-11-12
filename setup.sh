ad1189db18aca92cfe875ce7f5c895645b1a8651
Emiapwil (Gao Kai) <emiapwil@gmail.com>
2016-11-12 13:00:42 -0600

    Add the script to set up the environment
    Signed-off-by: Emiapwil (Gao Kai) <emiapwil@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+apt-get update
+
+apt-get -y install git apt-utils gcc wget
+apt-get -y install curl libcurl4-openssl-dev
+
+apt-get -y install python3 python3-pip
+
+apt-get -y install tmux
+
+pip3 install pycurl
+
+# Install bro
+
+BRO_DEB_PATH="http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/"
+BRO_APT_FILE="/etc/apt/sources.list.d/bro.list"
+
+sh -c "echo \'deb $BRO_DEB_PATH /\' > $BRO_APT_FILE"
+
+wget http://download.opensuse.org/repositories/network:bro/xUbuntu_14.04/Release.key
+apt-key add — < Release.key
+rm Release.key
+
+apt-get update
+apt-get -y install bro
+
+# Install demo applications
+
+WORKDIR=$(pwd)
+
+git clone https://github.com/emiapwil/demo-http tutorial-sc16
+make -C tutorial-sc16
+
+export PATH=$PATH:$WORKDIR/tutorial-sc16/
+
+# Install the mininet script
