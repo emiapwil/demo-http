f36e802c3c30fb4055a8f3e34222085c1c14a622
Kai Gao <emiapwil@gmail.com>
2016-11-14 10:09:48 -0800

    Update the server to use other backends
    Signed-off-by: Kai Gao <emiapwil@gmail.com>

--- curl_progress.c
+++ curl_progress.c
@@ -10,23 +10,23 @@ struct task_progress {
 	CURL *curl;
 
 	double timestamp;
+	curl_off_t to_be_downloaded;
+	curl_off_t to_be_uploaded;
 	curl_off_t downloaded;
 	curl_off_t uploaded;
 };
 
-static int report_progress(void *p,
-	 curl_off_t _d, curl_off_t downloaded,
-	 curl_off_t _u, curl_off_t uploaded) {
-	struct task_progress *progress = (struct task_progress *)p;
-	CURL *curl = progress->curl;
-
-	double current = 0;
+static int update_progress(struct task_progress *progress, double current, 
+				curl_off_t _d, curl_off_t downloaded,
+				curl_off_t _u, curl_off_t uploaded) {
 	long interval;
 	double download_speed, upload_speed;
-
-	curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &current);
+	double download_percentage, upload_percentage;
 
 	interval = (long)((current - progress->timestamp) * MICROSECOND_TO_SECOND);
+
+	progress->to_be_downloaded = _d;
+	progress->to_be_uploaded = _u;
 
 	if (interval < GRACEFUL_PERIOD) {
 		return 0;
@@ -35,14 +35,32 @@ static int report_progress(void *p,
 	download_speed = (double)(downloaded - progress->downloaded) / interval;
 	upload_speed = (double)(uploaded - progress->uploaded) / interval;
 
+	download_percentage = (downloaded) / (_d + 1e-10);
+	upload_percentage = (uploaded) / (_u + 1e-10);
+
 	progress->timestamp = current;
 	progress->downloaded = downloaded;
 	progress->uploaded = uploaded;
 
-	fprintf(stderr, "%.2f %.2f %.2f\r\n", current * MICROSECOND_TO_SECOND,
-					download_speed, upload_speed);
+	fprintf(stderr, "%.2f %.5f %.5f %.5f %.5f\r\n",
+			current * MICROSECOND_TO_SECOND,
+			download_speed, upload_speed,
+			download_percentage, upload_percentage);
 
 	return 0;
+}
+
+static int report_progress(void *p,
+	 curl_off_t _d, curl_off_t downloaded,
+	 curl_off_t _u, curl_off_t uploaded) {
+	struct task_progress *progress = (struct task_progress *)p;
+	CURL *curl = progress->curl;
+
+	double current = 0;
+
+	curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &current);
+
+	return update_progress(p, current, _d, downloaded, _u, uploaded);
 }
 
 int main(int argc, char** argv) {
@@ -62,6 +80,8 @@ int main(int argc, char** argv) {
 		progress.timestamp = 0;
 		progress.uploaded = 0;
 		progress.downloaded = 0;
+		progress.to_be_downloaded = 0;
+		progress.to_be_uploaded = 0;
 
 		curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
 
@@ -74,6 +94,11 @@ int main(int argc, char** argv) {
 
 		if (error != CURLE_OK) {
 			fprintf(stderr, "%s\n", curl_easy_strerror(error));
+		} else {
+			double diff = (GRACEFUL_PERIOD * 1.0) / MICROSECOND_TO_SECOND;
+			update_progress(&progress, progress.timestamp + diff,
+					progress.to_be_downloaded, progress.to_be_downloaded,
+					progress.to_be_uploaded, progress.to_be_uploaded);
 		}
 
 		/* always cleanup */
--- demohttpserver.py
+++ demohttpserver.py
@@ -6,20 +6,22 @@ from flask import Flask
 
 app = Flask("demo-http-server")
 
+def make_chunk(size):
+    return 'a' * (size - 2) + '\r\n'
+
 CHUNK_SIZE = 65536
-CHUNK = 'a' * CHUNK_SIZE
+CHUNK = make_chunk(CHUNK_SIZE)
 
 MIME_MAPPING = {
             "mp4": "video/mp4",
             "mkv": "video/divx",
             "avi": "video/divx",
             "zip": "application/x-zip-compressed",
-            "iso": "application/octetstream",
-            "txt": "text/txt",
+            "iso": "application/octet-stream",
+            "txt": "text/plain",
             "html": "text/html"
         }
 
-@app.route('/demo/<size>', defaults={'suffix': 'html'})
 @app.route('/demo/<size>.<suffix>')
 def supply(size, suffix):
     def generate(size):
@@ -28,16 +30,19 @@ def supply(size, suffix):
         for i in range(rnd):
             print('percentage: {:.2%}'.format(i * CHUNK_SIZE / size))
             yield CHUNK
-        yield 'a' * (size - rnd * CHUNK_SIZE)
+        yield CHUNK[rnd * CHUNK_SIZE - size:]
         print('percentage: {:.2%}'.format(1))
 
-    response = Response(generate(size), mimetype=MIME_MAPPING[suffix])
-    response.headers['content-length'] = size
+    headers = {}
+    headers['content-length'] = size
+    headers['content-type'] = "{}; name={}.{}".format(MIME_MAPPING[suffix], size, suffix)
+    headers['content-disposition'] = 'attachment; filename={}.{}'.format(size, suffix)
+    response = Response(generate(size), mimetype=MIME_MAPPING[suffix], headers=headers)
     return response
 
 if __name__ == '__main__':
     host = '127.0.0.1' if len(sys.argv) < 2 else sys.argv[1]
     port = 6666 if len(sys.argv) < 3 else int(sys.argv[2])
     CHUNK_SIZE = CHUNK_SIZE if len(sys.argv) < 4 else int(sys.argv[3])
-    CHUNK = 'a' * CHUNK_SIZE
+    CHUNK = make_chunk(CHUNK_SIZE)
     app.run(host=host, port=port)
